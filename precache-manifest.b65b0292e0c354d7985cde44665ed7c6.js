e1474f8d5fcc1b7f342b31fed2d147057f957867
Stephan <stephan287@gmail.com>
2020-10-16 02:25:12 -0400

    Updates

---
+++ precache-manifest.b65b0292e0c354d7985cde44665ed7c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8da4eff7042531a6b66098f5895515",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "d7d3bfe7a393708e44df",
+    "url": "/shopping-cart/static/css/main.cd66f383.chunk.css"
+  },
+  {
+    "revision": "0fe9d6c898594d65f84d",
+    "url": "/shopping-cart/static/js/2.4c84635a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/shopping-cart/static/js/2.4c84635a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d3bfe7a393708e44df",
+    "url": "/shopping-cart/static/js/main.e9543497.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping-cart/static/media/logo.5d5d9eef.svg"
+  }
+]);
